7bbd376dc6237c717094acdaa00b7fa2ef756e9f
Oscar Gonzalez <ocgonzalez126@gmail.com>
2018-08-24 18:05:55 -0700

    currently returns all indices when signal is above threshold. need to introduce nonOverlap period.

--- event_detection.py
+++ event_detection.py
@@ -1,5 +1,7 @@
 #--- Detect Ca events in Ophys data ---#
 
 def event_detection(data,thresh,nonOverlap,dt):
+	# find indices for detected Ca events #
+	fp = [ii for ii,val in enumerate(data>thresh) if val]
 
-	return fdfp
+	return fp
